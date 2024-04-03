4f7b665f8438741c70391734155854f815fcd03a
Barnabas Busa <busa.barnabas@gmail.com>
2024-04-03 11:04:39 +0200

    fix desc

--- terraform/devnet-0/hetzner/firewall.tf
+++ terraform/devnet-0/hetzner/firewall.tf
@@ -3,58 +3,66 @@ resource "hcloud_firewall" "machine_firewall" {
 
   # SSH
   rule {
-    direction  = "in"
-    protocol   = "tcp"
-    port       = "22"
-    source_ips = ["0.0.0.0/0", "::/0"]
+    description = "Allow SSH"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "22"
+    source_ips  = ["0.0.0.0/0", "::/0"]
   }
   # Nginx / Web
   rule {
-    direction  = "in"
-    protocol   = "tcp"
-    port       = "80"
-    source_ips = ["0.0.0.0/0", "::/0"]
+    description = "Allow HTTP"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "80"
+    source_ips  = ["0.0.0.0/0", "::/0"]
   }
 
   rule {
-    direction  = "in"
-    protocol   = "tcp"
-    port       = "443"
-    source_ips = ["0.0.0.0/0", "::/0"]
+    description = "Allow HTTPS"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "443"
+    source_ips  = ["0.0.0.0/0", "::/0"]
   }
 
   # Consensus layer p2p port
   rule {
-    direction  = "in"
-    protocol   = "tcp"
-    port       = "9000-9001"
-    source_ips = ["0.0.0.0/0", "::/0"]
+    description = "Allow consensus p2p port TCP"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "9000-9001"
+    source_ips  = ["0.0.0.0/0", "::/0"]
   }
 
   rule {
-    direction  = "in"
-    protocol   = "udp"
-    port       = "9000-9001"
-    source_ips = ["0.0.0.0/0", "::/0"]
+    description = "Allow consensus p2p port UDP"
+    direction   = "in"
+    protocol    = "udp"
+    port        = "9000-9001"
+    source_ips  = ["0.0.0.0/0", "::/0"]
   }
 
   # Execution layer p2p Port
   rule {
-    direction  = "in"
-    protocol   = "tcp"
-    port       = "30303"
-    source_ips = ["0.0.0.0/0", "::/0"]
+    description = "Allow execution p2p port TCP"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "30303"
+    source_ips  = ["0.0.0.0/0", "::/0"]
   }
 
   rule {
-    direction  = "in"
-    protocol   = "udp"
-    port       = "30303"
-    source_ips = ["0.0.0.0/0", "::/0"]
+    description = "Allow execution p2p port UDP"
+    direction   = "in"
+    protocol    = "udp"
+    port        = "30303"
+    source_ips  = ["0.0.0.0/0", "::/0"]
   }
 
   # Allow all outbound traffic
   rule {
+    description     = "Allow all outbound traffic TCP"
     direction       = "out"
     protocol        = "tcp"
     port            = "1-65535"
@@ -62,6 +70,7 @@ resource "hcloud_firewall" "machine_firewall" {
   }
 
   rule {
+    description     = "Allow all outbound traffic UDP"
     direction       = "out"
     protocol        = "udp"
     port            = "1-65535"
@@ -69,6 +78,7 @@ resource "hcloud_firewall" "machine_firewall" {
   }
 
   rule {
+    description     = "Allow all outbound traffic ICMP"
     direction       = "out"
     protocol        = "icmp"
     destination_ips = ["0.0.0.0/0", "::/0"]
