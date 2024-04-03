b7eae3340c1efc1a4c769c210522d3cc3ae5c057
Barnabas Busa <busa.barnabas@gmail.com>
2024-04-03 10:45:15 +0200

    feat: add hetzner fw

---
+++ terraform/devnet-0/hetzner/firewall.tf
@@ -0,0 +1,77 @@
+resource "hcloud_firewall" "machine_firewall" {
+  name = "${var.ethereum_network}-firewall"
+  
+  # SSH
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "22"
+    source_ips = ["0.0.0.0/0","::/0"]
+  }
+  # Nginx / Web
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "80"
+    source_ips = ["0.0.0.0/0","::/0"]
+  }
+
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "443"
+    source_ips = ["0.0.0.0/0","::/0"]
+  }
+
+  # Consensus layer p2p port
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "9000-9001"
+    source_ips = ["0.0.0.0/0","::/0"]
+  }
+
+  rule {
+    direction = "in"
+    protocol  = "udp"
+    port      = "9000-9001"
+    source_ips = ["0.0.0.0/0","::/0"]
+  }
+
+  # Execution layer p2p Port
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "30303"
+    source_ips = ["0.0.0.0/0","::/0"]
+  }
+
+  rule {
+    direction = "in"
+    protocol  = "udp"
+    port      = "30303"
+    source_ips = ["0.0.0.0/0","::/0"]
+  }
+
+  # Allow all outbound traffic
+  rule {
+    direction = "out"
+    protocol  = "tcp"
+    port      = "1-65535"
+    destination_ips = ["0.0.0.0/0","::/0"]
+  }
+
+  rule {
+    direction = "out"
+    protocol  = "udp"
+    port      = "1-65535"
+    destination_ips = ["0.0.0.0/0","::/0"]
+  }
+
+  rule {
+    direction = "out"
+    protocol  = "icmp"
+    destination_ips = ["0.0.0.0/0","::/0"]
+  }
+}
+
