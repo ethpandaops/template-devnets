a795e57e20bff5deb8f8a34b8e971494e216bdb1
Stefan <stefan@starflinger.eu>
2025-09-16 20:29:40 +0200

    fix plugin-add typo

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe 
+#!/bin/bash -xe
 
 # Uninstall previous asdf plugins that shouldn't be managed anymore under asdf
 asdf uninstall ansible || true
@@ -9,9 +9,9 @@ asdf plugin add age https://github.com/threkk/asdf-age.git || true
 asdf plugin add shellcheck https://github.com/luizm/asdf-shellcheck.git || true
 asdf plugin add sops https://github.com/feniix/asdf-sops.git || true
 asdf plugin add terraform https://github.com/asdf-community/asdf-hashicorp.git || true
-asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git || true
-asdf plugin-add python || true
-asdf plugin-add yq https://github.com/sudermanjr/asdf-yq.git || true
+asdf plugin add helm https://github.com/Antiarchitect/asdf-helm.git || true
+asdf plugin add python || true
+asdf plugin add yq https://github.com/sudermanjr/asdf-yq.git || true
 asdf plugin add awscli || true
 
 asdf install
